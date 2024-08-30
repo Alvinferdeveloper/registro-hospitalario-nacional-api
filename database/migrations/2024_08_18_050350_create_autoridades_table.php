fe20fec8d226ffc30718ddfb3f1fdba35456b2b6
Albin <alvinfer67@gmail.com>
2024-08-30 11:07:41 -0600

    Adding field 'for' forn roles table

--- database/migrations/2024_08_18_050350_create_autoridades_table.php
+++ database/migrations/2024_08_18_050350_create_autoridades_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('for',['ADMIN', 'HEALTH_CARER']);
             $table->timestamps();
         });
     }

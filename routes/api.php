2d2b8f9552b5cd0fff9542755f25b7b19f6720bc
Albin <alvinfer67@gmail.com>
2024-08-31 19:28:56 -0600

    added: ruta para registro de usuario

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/auth/register',[AuthController::class, 'register'] );
+

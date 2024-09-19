644a5ea4e050b0a72bb0db0976ff58a328b66c77
Albin <alvinfer67@gmail.com>
2024-09-19 17:47:14 -0600

    update: setting default date in field date

--- database/migrations/2024_08_18_050217_create_consultas_table.php
+++ database/migrations/2024_08_18_050217_create_consultas_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -17,7 +18,7 @@ return new class extends Migration
             $table->foreign('patient_id')->references('id')->on('patients');
             $table->uuid('health_carer_id');
             $table->foreign('health_carer_id')->references('id')->on('health_carers');
-            $table->date('date');
+            $table->date('date')->default(DB::raw('CURRENT_DATE'));
             $table->longText('summary');
             $table->longText('diagnosis');
             $table->longText('plan');

a01f6e0f21a534871578aac8004b2439f6da0963
Albin <alvinfer67@gmail.com>
2024-10-03 22:18:35 -0600

    update: new second hospital in the migration

--- database/seeders/hospitalSeeder.php
+++ database/seeders/hospitalSeeder.php
@@ -26,6 +26,16 @@ class hospitalSeeder extends Seeder
             'lng' => '-86.87784176747388'
         ]);
 
+        Hospital::create([
+            'name' => 'Hospital Militar',
+            'address_id' => $address->id,
+            'phone_number' => '0050581217545',
+            'email' => 'HospMilitar@gmail.com',
+            'emergency_number' => '0050581217545',
+            'lat' => '12.133736',
+            'lng' => '-86.278626'
+        ]);
+
 
 
     }
